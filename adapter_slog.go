442b7ad16176daab10c6238afa9310506f1a342d
Mikhail Knyazhev <markus621@yandex.com>
2026-01-20 08:48:26 +0300

    fix slog adapter

--- adapter_slog.go
+++ adapter_slog.go
@@ -1,68 +1,57 @@
 /*
- *  Copyright (c) 2024-2025 Mikhail Knyazhev <markus621@yandex.ru>. All rights reserved.
+ *  Copyright (c) 2024-2026 Mikhail Knyazhev <markus621@yandex.com>. All rights reserved.
  *  Use of this source code is governed by a BSD 3-Clause license that can be found in the LICENSE file.
  */
 
 package logx
 
 import (
-	"context"
 	"io"
 	"log/slog"
 	"os"
+	"sync/atomic"
 )
 
-var slogLevelMap = map[uint32]slog.Level{
-	LevelError: slog.LevelError,
-	LevelWarn:  slog.LevelWarn,
-	LevelInfo:  slog.LevelInfo,
-	LevelDebug: slog.LevelDebug,
-}
-
 type adapterSlog struct {
-	level   slog.Level
-	handler func(w io.Writer, level slog.Level) slog.Handler
+	level   atomic.Uint32
+	handler func(w io.Writer) slog.Handler
 	log     *slog.Logger
 }
 
 func NewSLogJsonAdapter() Logger {
 	obj := &adapterSlog{
-		level: slog.LevelDebug,
-		handler: func(w io.Writer, level slog.Level) slog.Handler {
+		handler: func(w io.Writer) slog.Handler {
 			return slog.NewJSONHandler(w, &slog.HandlerOptions{
-				Level: level,
+				Level: slog.LevelDebug,
 			})
 		},
 	}
+	obj.level.Store(LevelDebug)
 	obj.SetOutput(os.Stdout)
 	return obj
 }
 
 func NewSLogStringAdapter() Logger {
 	obj := &adapterSlog{
-		level: slog.LevelDebug,
-		handler: func(w io.Writer, level slog.Level) slog.Handler {
+		handler: func(w io.Writer) slog.Handler {
 			return slog.NewTextHandler(w, &slog.HandlerOptions{
-				Level: level,
+				Level: slog.LevelDebug,
 			})
 		},
 	}
+	obj.level.Store(LevelDebug)
 	obj.SetOutput(os.Stdout)
 	return obj
 }
 
 func (v *adapterSlog) SetOutput(out io.Writer) {
-	v.log = slog.New(v.handler(out, v.level))
+	v.log = slog.New(v.handler(out))
 }
 
 func (v *adapterSlog) SetFormatter(_ Formatter) {}
 
 func (v *adapterSlog) SetLevel(l uint32) {
-	level, ok := slogLevelMap[l]
-	if !ok {
-		level = slog.LevelDebug
-	}
-	v.log.Enabled(context.TODO(), level)
+	v.level.Store(l)
 }
 
 func (v *adapterSlog) Fatal(message string, args ...interface{}) {
@@ -71,17 +60,29 @@ func (v *adapterSlog) Fatal(message string, args ...interface{}) {
 }
 
 func (v *adapterSlog) Error(message string, args ...interface{}) {
+	if v.level.Load() < LevelError {
+		return
+	}
 	v.log.Error(message, args...)
 }
 
 func (v *adapterSlog) Warn(message string, args ...interface{}) {
+	if v.level.Load() < LevelWarn {
+		return
+	}
 	v.log.Warn(message, args...)
 }
 
 func (v *adapterSlog) Info(message string, args ...interface{}) {
+	if v.level.Load() < LevelInfo {
+		return
+	}
 	v.log.Info(message, args...)
 }
 
 func (v *adapterSlog) Debug(message string, args ...interface{}) {
+	if v.level.Load() < LevelDebug {
+		return
+	}
 	v.log.Debug(message, args...)
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,12 @@
 module go.osspkg.com/logx
 
-go 1.24.0
+go 1.24.6
 
 require (
 	github.com/mailru/easyjson v0.9.1
 	go.osspkg.com/casecheck v0.3.0
-	go.osspkg.com/ioutils v0.7.0
-	go.osspkg.com/syncing v0.4.0
+	go.osspkg.com/ioutils v0.7.4
+	go.osspkg.com/syncing v0.4.3
 )
 
 require github.com/josharian/intern v1.0.0 // indirect
